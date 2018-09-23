982b5c659c75aa5a460fe5a3f7ca7594c61d6423
widiamka <43339745+widiamka@users.noreply.github.com>
2018-09-23 10:15:32 +0700

    Add files via upload

---
+++ PBO3_10117082_Latihan4_Kambing/src/pbo3_10117082_latihan4_kambing/Kambing.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117082_latihan4_kambing;
+
+/**
+ *
+ * @author Nama : WIDIAMEGA K
+ *         Kelas : IF3
+ *         NIM    : 10117082
+ */
+public class Kambing {
+    public void tambahKambing(){
+       //deklarasi variabel lokal
+       int jumlahKambing = 0;
+       
+       jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing Setelah Ditambah : " +jumlahKambing);
+        
+    
+    }
+}
---
+++ PBO3_10117082_Latihan4_Kambing/src/pbo3_10117082_latihan4_kambing/PBO3_10117082_Latihan4_Kambing.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117082_latihan4_kambing;
+
+/**
+ *
+ * @author Nama : WIDIAMEGA K
+ *         Kelas : IF3
+ *         NIM : 10117082
+ */
+public class PBO3_10117082_Latihan4_Kambing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
